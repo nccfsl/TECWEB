f0d010689f266275e7153d8c8ef642e629fd1586
Niccolò Fasolo <niccolofasolo@gmail.com>
2022-12-21 08:37:33 +0100

    finished checks on post fields

--- LAB_15DIC/nuovo_giocatore.php
+++ LAB_15DIC/nuovo_giocatore.php
@@ -12,6 +12,17 @@ $nome = '';
 $capitano = '';
 $dataNascita = '';
 $luogo = '';
+$altezza = '';
+
+$squadra = '';
+$maglia = '';
+
+$ruolo = '';
+$magliaNazionale = '';
+
+$punti = '';
+$riconoscimenti = '';
+$note = '';
 
 function pulisciInput($value) { // l'ordine di queste istruzioni è fondamentale
     $value = trim($value);
@@ -30,34 +41,115 @@ function pulisciNote($value) {
 }
 
 if (isset($_POST['submit'])) {
+    // Controllo NOME
     $nome = pulisciInput($_POST['nome']);
     if (strlen($nome) == 0) {
         $messaggiPerForm .= '<li>Inserire Nome e Cognome</li>';
     }
     else {
-        if (preg_match("/\d{4}\-\d{2}\-\d{2}/", $nome)) {
+        if (preg_match("/\d/", $nome)) {
             $messaggiPerForm .= '<li>Nome e Cognome non possono contenere numeri</li>';
         }
     }
-
+    // Controllo CAPITANO
     $capitano = pulisciInput($_POST['capitano']);
-
+    // Controllo DATANASCITA
     $dataNascita = pulisciInput($_POST['dataNascita']);
     if (strlen($dataNascita) == 0) {
-        $messaggiPerForm .= '<li>Inserire Data di Nascita</li>';
+        $messaggiPerForm .= '<li>Inserire Data di nascita</li>';
     }
     else {
-        if (preg_match("/\d/", $dataNascita)) {
-            $messaggiPerForm .= '<li>Data di Nascita in formato non corretto</li>';
+        if (preg_match("/\d{4}\-\d{2}\-\d{2}/", $dataNascita)) { // regex da cambiare
+            $messaggiPerForm .= '<li>Data di nascita in formato non corretto</li>';
         }
     }
-    // ...(tutto il resto è da fare)
+    // Controllo LUOGO
+    $luogo = pulisciInput($_POST['luogo']);
+    if (strlen($luogo) == 0) {
+        $messaggiPerForm .= '<li>Inserire Luogo di nascita</li>';
+    }
+    else {
+        if (preg_match("/\d/", $luogo)) {
+            $messaggiPerForm .= '<li>Luogo di nascita non può contenere numeri</li>';
+        }
+    }
+    // Controllo ALTEZZA
+    $altezza = pulisciInput($_POST['altezza']);
+    if (strlen($altezza) == 0) {
+        $messaggiPerForm .= '<li>Inserire Altezza</li>';
+    }
+    else {
+        if (preg_match("/[A-Za-z]+/", $altezza)) {
+            $messaggiPerForm .= '<li>Altezza non può contenere lettere</li>';
+        }
+    }
+    // Controllo SQUADRA
+    $squadra = pulisciInput($_POST['squadra']);
+    if (strlen($squadra) == 0) {
+        $messaggiPerForm .= '<li>Inserire Squadra</li>';
+    }
+    else {
+        if (preg_match("/\W/", $squadra)) {
+            $messaggiPerForm .= '<li>Squadra deve contenere solo lettere e numeri</li>';
+        }
+    }
+    // Controllo MAGLIA
+    $maglia = pulisciInput($_POST['maglia']);
+    if (strlen($maglia) == 0) {
+        $messaggiPerForm .= '<li>Inserire Maglia</li>';
+    }
+    else {
+        if (preg_match("/[A-Za-z]+/", $maglia)) {
+            $messaggiPerForm .= '<li>Maglia non può contenere lettere</li>';
+        }
+    }
+    // Controllo RUOLO
+    $ruolo = pulisciInput($_POST['ruolo']);
+    if (strlen($ruolo) == 0) {
+        $messaggiPerForm .= '<li>Inserire Ruolo</li>';
+    }
+    else {
+        if (preg_match("/^\b(Palleggiatore|Libero|Centrale|Schiacciatore|Opposto)/", $ruolo)) {
+            $messaggiPerForm .= '<li>Ruolo può essere solo: Palleggiatore, Libero, Centrale, Schiacciatore, Opposto</li>';
+        }
+    }
+    // Controllo MAGLIANAZIONALE
+    $magliaNazionale = pulisciInput($_POST['magliaNazionale']);
+    if (strlen($magliaNazionale) == 0) {
+        $messaggiPerForm .= '<li>Inserire Maglia in nazionale</li>';
+    }
+    else {
+        if (preg_match("/[A-Za-z]+/", $maglia)) {
+            $messaggiPerForm .= '<li>Maglia in nazionale non può contenere lettere</li>';
+        }
+    }
+    // Controllo PUNTI/RICEZIONI
+    $punti = pulisciInput($_POST['punti']);
+    if (strlen($punti) == 0) {
+        $messaggiPerForm .= '<li>Inserire Punti/Ricezioni</li>';
+    }
+    else {
+        if (preg_match("/[A-Za-z]+/", $punti)) {
+            $messaggiPerForm .= '<li>Punti/Ricezioni non può contenere lettere</li>';
+        }
+    }
+    // Controllo RICONOSCIMENTI
+    $riconoscimenti = pulisciNote($_POST['riconoscimenti']);
+    // Controllo NOTE
+    $note = pulisciNote($_POST['note']);
 }
 
 $paginaHTML = str_replace('<messaggiForm />', $messaggiPerForm, $paginaHTML);
 $paginaHTML = str_replace('<valoreNome />', $nome, $paginaHTML);
 $paginaHTML = str_replace('<valData />', $dataNascita, $paginaHTML);
-$paginaHTML = str_replace('<valLuogo />', $luogo, $paginaHTML)
-// ... (tutto il resto è da fare)
-
+$paginaHTML = str_replace('<valLuogo />', $luogo, $paginaHTML);
+$paginaHTML = str_replace('<valoreAltezza />', $altezza, $paginaHTML);
+$paginaHTML = str_replace('<valoreSquadra />', $squadra, $paginaHTML);
+$paginaHTML = str_replace('<valoreMaglia />', $maglia, $paginaHTML);
+$paginaHTML = str_replace('<valoreRuolo />', $ruolo, $paginaHTML);
+$paginaHTML = str_replace('<valoreMagliaNazionale />', $magliaNazionale, $paginaHTML);
+$paginaHTML = str_replace('<valorePunti />', $punti, $paginaHTML);
+$paginaHTML = str_replace('<valoreRiconoscimenti />', $riconoscimenti, $paginaHTML);
+$paginaHTML = str_replace('<valoreNote />', $note, $paginaHTML);
+echo $paginaHTML;
 ?>
